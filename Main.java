85ce77dea7e140c700fa54e6d47782d16a558cba
Arun rPatap Singh <arunsingh28aug.as@gmail.com>
2022-02-05 01:35:25 +0530

    Matrix sum

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
-public class Main {
-    String name = "Arun";
+import java.util.*;
 
-    public static void main(String[] args) {
-        System.out.print("Hello");
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Welcome to Matrix Calculator!");
+        System.out.println("Please enter the number of rows and columns of the matrix:");
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        System.out.println("Please enter the elements of the matrix:");
+        int[][] matrix = new int[rows][cols];
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                
+            }
+        }
     }
 }
