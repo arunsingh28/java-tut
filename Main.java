4c9b7018a7a5cb67ad6c71f8986d9dd3d29078a3
Arun rPatap Singh <arunsingh28aug.as@gmail.com>
2022-02-05 01:46:51 +0530

    Date

--- Main.java
+++ Main.java
@@ -2,17 +2,8 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args){
-        System.out.println("Welcome to Matrix Calculator!");
-        System.out.println("Please enter the number of rows and columns of the matrix:");
-        Scanner sc = new Scanner(System.in);
-        int rows = sc.nextInt();
-        int cols = sc.nextInt();
-        System.out.println("Please enter the elements of the matrix:");
-        int[][] matrix = new int[rows][cols];
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                
-            }
-        }
+        Date now = new Date();
+        System.out.println("Date :"+ now);
+       
     }
 }
