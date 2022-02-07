5f2c7af906f3eade9ca937469b5b3b1ffe75d519
Arun rPatap Singh <arunsingh28aug.as@gmail.com>
2022-02-08 00:27:30 +0530

    implicit casting

--- Main.java
+++ Main.java
@@ -6,10 +6,11 @@ public class Main {
     // System.out.println("Date :"+ now.getDate());
     // }
 
-    // array
+    // implicit casting
+    // byte > short > int > long > float > double > char > boolean
     public static void main(String[] args) {
-        int[] numbers = { 1, 40, 5, 7, 10 };
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
+    short a = 10;
+    int b = a;
+    System.out.println(b);  
     }
 }
