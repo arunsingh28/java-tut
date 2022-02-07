7e16cf2fce3f94ee5aa296fe41d88058133a1b3c
Arun rPatap Singh <arunsingh28aug.as@gmail.com>
2022-02-07 23:49:53 +0530

    string starting

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 import java.util.*;
 
 public class Main{
+    // public static void main(String[] args){
+    //     Date now = new Date();
+    //     System.out.println("Date :"+ now.getDate());
+    // }
+    
+    // string 
     public static void main(String[] args){
-        Date now = new Date();
-        System.out.println("Date :"+ now.getDate());
-        
+        String message = "Hello World" + "!!";
+        message.endsWith(message);
+        System.out.println(message);
     }
 }
