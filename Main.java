5395f3089b413f0483dc137710063795471fd794
Arun rPatap Singh <arunsingh28aug.as@gmail.com>
2022-02-08 00:37:36 +0530

    converting string to int

--- Main.java
+++ Main.java
@@ -1,12 +1,10 @@
 import java.util.*;
 
 public class Main {
-    // public static void main(String[] args){
-    // Date now = new Date();
-    // System.out.println("Date :"+ now.getDate());
-    // }
-
-    // implicit casting
-    // byte > short > int > long > float > double > char > boolean
+    public static void main(String[] args) {
+        String x = "1";
+        int y = Integer.parseInt(x);
+        System.out.println(y);
+    }
 
 }
