20360094696db03e9e9eec261f6b8301501d1f26
Arun rPatap Singh <arunsingh28aug.as@gmail.com>
2022-02-20 00:09:36 +0530

    FIX:addition for rows

--- Matrix.java
+++ Matrix.java
@@ -27,9 +27,12 @@ public class Matrix {
       }
 
       // adding two rows
-      for(int i=0;i<matrix.length;i++){
-         
+      for (int i = 0; i < matrix.length; i++) {
+         for (int j = 0; j < matrix.length; j++) {
+            int sum = 0;
+            sum = matrix[i][j] + matrix[i][j];
+            System.out.println(i + j + "=" + sum);
+         }
       }
-
    }
 }
