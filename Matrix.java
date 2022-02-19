3cc6339d1ef6528a9b9d3c6c1df4e904832b64e6
Arun rPatap Singh <arunsingh28aug.as@gmail.com>
2022-02-19 23:59:58 +0530

    more dynamic

--- Matrix.java
+++ Matrix.java
@@ -3,21 +3,26 @@ import java.util.Scanner;
 
 public class Matrix {
    public static void main(String[] args) {
-      int[][] matrix = new int[3][3];
       Scanner sc = new Scanner(System.in);
+      System.out.print("Enter number of rows: ");
+      int rows = sc.nextInt();
+      System.out.print("Enter number of columns: ");
+      int cols = sc.nextInt();
+      int[][] matrix = new int[rows][cols];
       // input
       System.out.println("Enter the elements of the matrix");
-      for (int i = 0; i <= matrix.length; i++) {
-         for (int j = 0; j <= matrix.length; j++) {
-            System.out.println("Enter element " + i + " " + j);
+      for (int i = 0; i < matrix.length; i++) {
+         for (int j = 0; j < matrix.length; j++) {
+            System.out.print("Enter element " + i + " " + j + ":");
             matrix[i][j] = sc.nextInt();
          }
       }
       // output
       System.out.println("Output");
-      for (int i = 0; i <= 3; i++) {
-         for (int j = 0; j <= 3; j++) {
-            matrix[i][j] = sc.nextInt();
+      for (int i = 0; i < matrix.length; i++) {
+         for (int j = 0; j < matrix.length; j++) {
+            System.out.print("Matrix " + i + " " + j + ":");
+            System.out.println(matrix[i][j]);
          }
       }
 
